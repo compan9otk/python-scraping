cca69d108916a2234f1a6c1da8e1de48058120a5
compan9otk <6ri6rikatatu6ri@gmail.com>
2022-03-13 13:12:10 +0900

    チェックボックス・テキストボックス・スライダーを追加

--- Dash/sample01.py
+++ Dash/sample01.py
@@ -35,8 +35,29 @@ app.layout = html.Div([
             {'label': '田中', 'value': 'tanaka'}
         ],
         value='suzuki'
+    ),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': '佐藤', 'value': 'sato'},
+            {'label': '鈴木', 'value': 'suzuki'},
+            {'label': '田中', 'value': 'tanaka'},
+        ],
+        value=['suzuki','tanaka']
+    ),
+
+    html.Label('Text Input'),
+    dcc.Input(value='佐藤', type='text'),
+
+    html.Label('Slider'),
+    dcc.Slider(
+      min=0,
+      max=5,
+      marks={i:str(i) for i in range(1,6)},
+      value=3
     )
-])
+], style={'columnCount':2})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
